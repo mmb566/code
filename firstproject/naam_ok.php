437efd5b04539d61d87f3004fdeda31897f8a780
mmb566 <45395422+mmb566@users.noreply.github.com>
2019-03-14 12:36:04 +0100

    Add files via upload

---
+++ firstproject/naam_ok.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulier</title>
+</head>
+<body>
+
+<h2>Vul u naam in.</h2>
+
+<form action="post.php" method="POST">
+    naam:
+    <input type="text" name="naam" id="naam">
+    <br/>
+    <br/>
+    <input type="submit" name="verzenden">
+</form>
+
+
+
+</body>
+</html>
---
+++ firstproject/post.php
@@ -0,0 +1,9 @@
+<?php
+$naam = $_POST['naam'];
+echo $naam;
+
+echo "</br>";
+
+echo "<button><a href= naam_ok.php>terug</a></button>";
+
+?>
